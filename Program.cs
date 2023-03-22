64020f4c26a6d0efa89eaccc320745ab5bbdd5fd
Ruth Sunagua Vargas <ruth.sunagua.vargas@avanade.com>
2023-03-22 15:26:47 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using BethanysPieShopHRM;
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime (1979, 1, 16), 25);
+
+bethany.DisplayEmployeeDetails();
+
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork(5);
+bethany.PerformWork();
+
+double receiveWageBethany = bethany.ReciveWage(true);
+Console.WriteLine($"Wage paid (message from Program): {receiveWageBethany}");
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30);
+
+george.DisplayEmployeeDetails();
+george.PerformWork();
+george.PerformWork();
+george.PerformWork(3);
+george.PerformWork();
+george.PerformWork(8);
+
+var receivedWageGeorge = george.ReciveWage(true);
